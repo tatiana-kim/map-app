b3247273eb30420950caaa12d1dcc231852bce87
tatiana-kim <kte.ent@gmail.com>
2022-01-17 00:11:25 -0900

    hid settings' key

---
+++ world/configs.py
@@ -0,0 +1,1 @@
+SECRETKEY = 'django-insecure-+$mya5_@!x9-qzad4o!!7n3k4mx6t1x&0kofa8)+=*w2zr(@1)'
--- world/world/settings.py
+++ world/world/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from configs import SECRETKEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+$mya5_@!x9-qzad4o!!7n3k4mx6t1x&0kofa8)+=*w2zr(@1)'
+SECRET_KEY = SECRETKEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -33,7 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'leaflet',
+    # 'leaflet',
     'rest_framework',
     'rest_framework_gis',
     'django.contrib.gis',
